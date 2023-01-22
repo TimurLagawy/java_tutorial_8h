78f62215b3c44fdf5550e2617040d2bee8bb6998
Timur Lagawy <timurtagirovich@mail.ru>
2023-01-22 20:13:27 +0100

    lesson_8

--- src/test/PersonTest.java
+++ src/test/PersonTest.java
@@ -8,17 +8,19 @@ import static org.junit.Assert.assertEquals;
 public class PersonTest {
 
     @Test
-    public void shouldReturnHelloWorld(){
+    public void shouldReturnHelloWorld() {
         Person timur = new Person();
         assertEquals("Hello World", timur.helloWorld());
     }
+
     @Test
     public void shouldReturnHelloTimur() {
         Person person = new Person();
         assertEquals("Hello Timur", person.hello("Timur"));
     }
+
     @Test
-            public void souldreturnNumberOfPersons(){
+    public void souldreturnNumberOfPersons() {
         Person person1 = new Person();
         Person Person2 = new Person();
         Person person3 = new Person();
@@ -27,11 +29,54 @@ public class PersonTest {
     }
 
     @Test
-    public void demonstrateBoolean(){
+    public void shouldReturnOfPersonsInLoop() {
+        Person person1;
+        int i = 0;
+
+        while(i < 4){
+            person1 = new Person();
+            i++;
+        }
+        /*for (int i = 0; i < 4; i ++) {
+            person1 = new Person();
+        }*/
+        assertEquals(4, Person.numberOfPersons());
+        System.out.println( "true  " + Person.numberOfPersons() );
+    }
+
+    @Test
+    public void demonstrateBoolean() {
+        int i = 4;
+        int j = 3;
         boolean monday = false;
         boolean raining = true;
-        if(monday && raining){
-            drinkBeer();
+
+        //if(shouldStayAtHome(i && j && monday && raining) )
+        if (shouldStayAtHome(i, j, monday, raining)) {
+            stayAtHome();
         }
+        if (i == j) {
+            raining = true;
+        }
+        if (monday || raining) {
+            i = 34;
+            j = 91;
+            i = i + j;
+            stayAtHome();
+        } else if (j > i) {
+            i++;
+            j = 56;
+        } else if (i != j) {
+            i = 8;
+        } else {
+            j--;
+        }
+    }
+
+    private void stayAtHome() {
+    }
+
+    private boolean shouldStayAtHome(int i, int j, boolean monday, boolean raining) {
+         return i < j || (monday && raining) || j > 37;
     }
 }
